454101d1fe37cccc427780057832105aab9d285d
Pyro1618 <45923976+Pyro1618@users.noreply.github.com>
2019-01-10 19:43:44 +0200

    Add files via upload

---
+++ Program1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercice_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int secondsTotal = 0;
+
+            do
+            {
+                Console.WriteLine("Write a positive number");
+                secondsTotal = Convert.ToInt32(Console.ReadLine());
+                if (secondsTotal <= 0)
+                {
+                    Console.Write("idiot !!!");
+                }
+            } while (secondsTotal <= 0);
+
+            int days = secondsTotal / 86400;
+            secondsTotal = secondsTotal % 86400;
+            int hours = secondsTotal / 3600;
+            secondsTotal = secondsTotal % 3600;
+            int minutes = secondsTotal / 60;
+            int seconds = secondsTotal % 60;
+
+            Console.WriteLine($"{days}days, {hours}hours, {minutes}minutes, {seconds}seconds");
+        }
+    }
+}
