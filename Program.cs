25cd3ca5775bd804ba278ffc3edc278a90036527
Pyro1618 <45923976+Pyro1618@users.noreply.github.com>
2019-01-10 19:42:31 +0200

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -4,91 +4,32 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Exercice_1
+namespace Exercice_2
 {
     class Program
     {
         static void Main(string[] args)
         {
-            // First Part
-            int a = 0;
+            int secondsTotal = 0;
 
-            PrintPrimeOrNot(CheckNumBetw_0_1000(a));
-
-            // Second Part
-
-            int b = 0;
-            int c = 0;
-
-            a = CheckNumBetw_0_1000(a);
-            b = CheckNumBetw_0_1000(b);
-            c = CheckNumBetw_0_1000(c);
-
-//            (PrimeOrNot(a) && PrimeOrNot(b) && PrimeOrNot(c)) ? (Console.WriteLine($"={a + b + c} = {a} + {b} + {c}")) : Console.WriteLine("Not Primary Numbers")
-         if (PrimeOrNot(a) && PrimeOrNot(b) && PrimeOrNot(c))
-            {
-                Console.WriteLine($"{a + b + c} = {a} + {b} + {c}");
-            }
-            else
-            {
-                Console.WriteLine("Not Primary Numbers");
-            }
-
- 
-
-
-        }
-
-        private static bool PrimeOrNot(int a)
-        {
-            bool isPrime;
-            int m = 2;
-            while (a % m != 0)
-            {
-                m++;
-            }
-
-            if (m < a)
-            {
-                isPrime = false;
-            }
-            else
-            {
-                isPrime = true;
-            }
-
-            return isPrime;
-        }
-
-        private static int PrintPrimeOrNot(int a)
-        {
-            // check if number is prime?
-            int m = 2;
-            while (a % m != 0)
-            {
-                m++;
-            }
-
-            if (m < a)
-            {
-                Console.WriteLine($"{a} is NOT prime");
-            }
-            else
-            {
-                Console.WriteLine($"{a} is prime indeed");
-            }
-            return a;
-        }
-
-        private static int CheckNumBetw_0_1000(int a)
-        {
-            
             do
             {
-                Console.WriteLine("Please Write Number between 0 and 1000");
-                a = Convert.ToInt32(Console.ReadLine());
-            } while (a <= 0 || a >= 1000);
-            return a;
+                Console.WriteLine("Write a positive number");
+                secondsTotal = Convert.ToInt32(Console.ReadLine());
+                if (secondsTotal <= 0)
+                {
+                    Console.Write("idiot !!!");
+                }
+            } while (secondsTotal <= 0);
+
+            int days = secondsTotal / 86400;
+            secondsTotal = secondsTotal % 86400;
+            int hours = secondsTotal / 3600;
+            secondsTotal = secondsTotal % 3600;
+            int minutes = secondsTotal / 60;
+            int seconds = secondsTotal % 60;
+
+            Console.WriteLine($"{days}days, {hours}hours, {minutes}minutes, {seconds}seconds");
         }
     }
 }
